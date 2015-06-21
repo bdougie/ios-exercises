be6196d16fdd7dd29be4d2ec433c57f8150af346
Brian Douglas <ilikerobot@gmail.com>
2015-06-21 00:43:55 -0700

    passes loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,23 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSString *range = @"";
+    
+    if (number <= otherNumber) {
+        while (number <= otherNumber) {
+            range = [range stringByAppendingString:@(number).stringValue];
+            ++number;
+        }
+    } else if (number >= otherNumber) {
+        while (otherNumber <= number) {
+            range = [range stringByAppendingString:@(otherNumber).stringValue];
+            ++otherNumber;
+        }
+    } else {
+        range = @(number).stringValue;
+
+    }
+    return range;
 }
 
 @end
