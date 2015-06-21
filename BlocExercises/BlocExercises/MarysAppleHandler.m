cd9d2a5e6be3ea6fce95c8fba86ed67030560df5
Brian Douglas <ilikerobot@gmail.com>
2015-06-21 00:01:08 -0700

    passes conditionals

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -12,6 +12,18 @@
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
+    
+    if (dollars >= 5 && dollars < 6) {
+        itemToReturn = @"have some gum";
+    } else if (dollars >= 6 && dollars < 1000) {
+        itemToReturn = @"have an apple";
+    } else if ( dollars >= 1000 && dollars < 1000000000){
+        itemToReturn = @"have an Apple computer";
+    } else if (dollars >= 1000000000){
+        itemToReturn = @"have The Big Apple";
+    } else {
+        itemToReturn = @"get out of my store";
+    }
 
     /* WORK HERE */
     
